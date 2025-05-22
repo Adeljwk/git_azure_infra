66b30880265e1dcd1c3ac7f98c63df7a89086a2e
Ubuntu <student@adel-enablon.1nkoirkcj1cuzicxtpoxj5ixyf.parx.internal.cloudapp.net>
2025-05-22 08:38:18 +0000

    adding var

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "azure_infra" {
     source="./modules/azure_infra"
 
     ressource_name ="vartest1"
+    name_prefix = var.name
     vnet_cidrs =["10.0.0.0/16"]
     subnet_cidrs =["10.0.0.0/24"]
     open_ports=["80", "443"]
--- modules/azure_infra/main.tf
+++ modules/azure_infra/main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "adel" {
 }
 
 resource "azurerm_virtual_network" "adel" {
-  name                = "${var.ressource_name}-adel2-vnet"
+  name                = "${var.name_prefix}-adel2-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.adel.location
   resource_group_name = azurerm_resource_group.adel.name
 }
 
 resource "azurerm_subnet" "adel" {
-  name                 = "${var.ressource_name}-adel2-subnet"
+  name                 = "${var.name_prefix}-adel2-subnet"
   resource_group_name  = azurerm_resource_group.adel.name
   virtual_network_name = azurerm_virtual_network.adel.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -19,19 +19,19 @@ resource "azurerm_subnet" "adel" {
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "${var.ressource_name}-example-resources"
+  name     = "${var.name_prefix}-example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_network_security_group" "adel" {
-  name                = "${var.ressource_name}-acceptanceTestSecurityGroup1"
+  name                = "${var.name_prefix}-acceptanceTestSecurityGroup1"
   location            = azurerm_resource_group.adel.location
   resource_group_name = azurerm_resource_group.adel.name
 }
 
 resource "azurerm_network_security_rule" "example" {
   for_each = toset(var.open_ports)
-  name                        = "${var.ressource_name}-${each.key}-test123"
+  name                        = "${var.name_prefix}-${each.key}-test123"
   priority                    = 100+each.key
   direction                   = "Outbound"
   access                      = "Allow"
